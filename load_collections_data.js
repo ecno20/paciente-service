8a68f8c158eba3115249740edd6f1a1ada9520a3
Jonathan Díaz <144557398+ecno20@users.noreply.github.com>
2023-10-05 05:10:38 +0000

    Add files via upload

---
+++ load_collections_data.js
@@ -0,0 +1,22 @@
+// insert pacientes
+db.pacientes.insertMany( [
+   	{ 
+	   nombre: "Thor" 
+	}
+]);
+
+db.pacientes.insertMany( [
+   	{ 
+	   nombre: "Wiskas" 
+	}
+]);
+db.pacientes.insertMany( [
+   	{ 
+	   nombre: "Pelusa" 
+	}
+]);
+db.pacientes.insertMany( [
+   	{ 
+	   nombre: "Brandy" 
+	}
+]);
